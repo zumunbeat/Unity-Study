b8d3ae042ae757e18f4420c47e7d443e876de04b
miru0522 <71073251+miru0522@users.noreply.github.com>
2023-05-28 17:40:27 +0900

    B5 : LifeCycle
    23.05.28 Upload
    B5 : LifeCycle
    B5 : LifeCycle CS Script

---
+++ Park/B5LS.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B5LS : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake 되었습니다.");
+    }
+
+    void OnEnable()
+    {
+        
+    Debug.Log("활성")
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("aa");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("픽스된 업데이트");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("업데이트");
+    }
+
+    void LateUpdate()
+    {
+        Debug.Log("레이트 업데이트");
+    }
+
+    void OnDisable()
+    {
+        
+    Debug.Log("비활성")
+    }
+    void OnDestory()
+    {
+        Debug.Log("파괴됨");
+    }
+}

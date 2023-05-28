f5c16331dee166dd0024daf905eea581ab2ebb89
miru0522 <71073251+miru0522@users.noreply.github.com>
2023-05-28 21:52:09 +0900

    B7 : move_2
    23.05.28 upload
    B7 : move(2)
    target으로 물체 옮기는 4가지 방법

---
+++ Park/B7_move2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B7_move2 : MonoBehaviour
+{
+    Vector3 target = new Vector3(8, 1.5f, 0);
+    
+
+    void Update()
+    {
+        /*
+        transform.position = 
+            Vector3.MoveTowards(transform.position, target, 0.1f);
+*/
+       /* 
+        Vector3 velo = Vector3.zero;
+        transform.position = 
+            Vector3.SmoothDamp(transform.position, target
+                            , ref velo,0.1f);
+*/
+        /*
+        transform.position = 
+            Vector3.Lerp(transform.position,
+                            target, 0.1f);
+*/
+
+        transform.position = 
+            Vector3.Slerp(transform.position
+                            , target, 0.005f);
+
+
+
+    }
+}

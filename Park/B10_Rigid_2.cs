1a4c44fd1bcbe6a8bfc4ebc8c90537036ff8fd27
miru0522 <71073251+miru0522@users.noreply.github.com>
2023-05-29 18:29:11 +0900

    Add files via upload
    23.05.29 Upload
    B 10 : move objects with Vector
    Use component 'Rigidbody', class 'Vector3' . Vector3.Velocity set Vector force, Vector3.AddForce add Vector force, AddTorque add torque with vector

---
+++ Park/B10_Rigid_2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B10_Rigid_2 : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        //rigid.velocity = new Vector3(1, 2, 3);
+        //벡터값 만큼의 힘으로 지정
+        //rigid.AddForce(Vector3.up * 3, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            rigid.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+        //벡터값 만큼 힘을 가함
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"),
+                               0, Input.GetAxisRaw("Vertical"));
+        //rigid.AddForce(vec, ForceMode.Impulse);
+        rigid.AddTorque(vec);
+
+    }
+}
